ee3a8e9b5bbeb0baa83b512733c292dadb053a4e
Dipendra Subedi <subedid@mcmaster.ca>
2020-05-27 19:33:54 -0400

    added exporting functionality to request function and tested on browser

--- src/API/index.js
+++ src/API/index.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const URL = 'https://covid19.mathdro.id/api';
 
-var getData = async () =>{
-
+export const getData = async () =>{
+    try { //if fetch is successful
+        
+        const attempt = await axios.get(URL); //use library to make a request
+        return attempt;
+    } catch (error) { //if unsuccessful
+        
+    }
 }
--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import Cards from './seperate parts/Card/Cards';
 import Chart from './seperate parts/Chart/Chart';
 import Country from './seperate parts/Country/Country';
-
+import { getData } from './API'; 
 import styles from './App.module.css';
 
 class App extends React.Component{
+
+    async componentDidMount() {
+        const data = await getData();
+        console.log(data);
+    }
+
     render() {
         return (
             <div className= {styles.container}> 
