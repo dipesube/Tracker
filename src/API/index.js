93bca8e3cc56dd63648ef0f9d455d4246f06dfc0
Dipendra Subedi <subedid@mcmaster.ca>
2020-05-27 20:18:51 -0400

    cleaned up js object

--- src/API/index.js
+++ src/API/index.js
@@ -5,8 +5,15 @@ const URL = 'https://covid19.mathdro.id/api';
 export const getData = async () =>{
     try { //if fetch is successful
         
-        const attempt = await axios.get(URL); //use library to make a request
-        return attempt;
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(URL); //use library to make a request
+        const parsedData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        }
+
+        return parsedData;
     } catch (error) { //if unsuccessful
         
     }
